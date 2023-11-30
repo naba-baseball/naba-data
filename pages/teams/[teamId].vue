b3ca4b2a986a5e15e8995b0a963c19be527d18b0
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-11-29 19:39:40 -0700

    basic sorting

--- pages/teams/[teamId].vue
+++ pages/teams/[teamId].vue
@@ -41,9 +41,11 @@ const splitOptions = [
 
 ]
 const rosterType = ref(2)
+const sortBy = ref('position')
 const filteredPlayers = computed(() => {
   const primaryRoster = team.value.data.roster.filter(entry => entry.list_id === rosterType.value).map(entry => entry.player_id)
-  return players.value.data?.filter(player => primaryRoster.includes(player.player_id)) ?? []
+  return players.value.data?.filter(player => primaryRoster.includes(player.player_id))
+    .toSorted((a, b) => a[sortBy.value] - b[sortBy.value]) ?? []
 })
 
 const battingLabels = [
@@ -90,107 +92,6 @@ const ratingsLabels = computed(() => {
   }
   return []
 })
-
-const ratingsKeys = {
-  batting: {
-    overall: [
-      'batting_ratings_overall_babip',
-      'batting_ratings_overall_contact',
-      'batting_ratings_overall_eye',
-      'batting_ratings_overall_gap',
-      'batting_ratings_overall_hp',
-      'batting_ratings_overall_power',
-      'batting_ratings_overall_strikeouts',
-    ],
-    vsl: [
-      'batting_ratings_vsl_babip',
-      'batting_ratings_vsl_contact',
-      'batting_ratings_vsl_eye',
-      'batting_ratings_vsl_gap',
-      'batting_ratings_vsl_hp',
-      'batting_ratings_vsl_power',
-      'batting_ratings_vsl_strikeouts',
-    ],
-    vsr: [
-      'batting_ratings_vsr_babip',
-      'batting_ratings_vsr_contact',
-      'batting_ratings_vsr_eye',
-      'batting_ratings_vsr_gap',
-      'batting_ratings_vsr_hp',
-      'batting_ratings_vsr_power',
-      'batting_ratings_vsr_strikeouts',
-    ],
-    talent: [
-      'batting_ratings_talent_babip',
-      'batting_ratings_talent_contact',
-      'batting_ratings_talent_eye',
-      'batting_ratings_talent_gap',
-      'batting_ratings_talent_hp',
-      'batting_ratings_talent_power',
-      'batting_ratings_talent_strikeouts',
-    ],
-  },
-  pitching: {
-    overall: [
-      'pitching_ratings_overall_balk',
-      'pitching_ratings_overall_control',
-      'pitching_ratings_overall_hp',
-      'pitching_ratings_overall_movement',
-      'pitching_ratings_overall_stuff',
-      'pitching_ratings_overall_wild_pitch',
-    ],
-    vsl: [
-      'pitching_ratings_vsl_balk',
-      'pitching_ratings_vsl_control',
-      'pitching_ratings_vsl_hp',
-      'pitching_ratings_vsl_movement',
-      'pitching_ratings_vsl_stuff',
-      'pitching_ratings_vsl_wild_pitch',
-    ],
-    vsr: [
-      'pitching_ratings_vsr_balk',
-      'pitching_ratings_vsr_control',
-      'pitching_ratings_vsr_hp',
-      'pitching_ratings_vsr_movement',
-      'pitching_ratings_vsr_stuff',
-      'pitching_ratings_vsr_wild_pitch',
-    ],
-    pitches: [
-      'pitching_ratings_pitches_changeup',
-      'pitching_ratings_pitches_circlechange',
-      'pitching_ratings_pitches_curveball',
-      'pitching_ratings_pitches_cutter',
-      'pitching_ratings_pitches_fastball',
-      'pitching_ratings_pitches_forkball',
-      'pitching_ratings_pitches_knuckleball',
-      'pitching_ratings_pitches_knucklecurve',
-      'pitching_ratings_pitches_screwball',
-      'pitching_ratings_pitches_sinker',
-      'pitching_ratings_pitches_slider',
-      'pitching_ratings_pitches_splitter',
-    ],
-    talent: [
-      'pitching_ratings_pitches_talent_changeup',
-      'pitching_ratings_pitches_talent_circlechange',
-      'pitching_ratings_pitches_talent_curveball',
-      'pitching_ratings_pitches_talent_cutter',
-      'pitching_ratings_pitches_talent_fastball',
-      'pitching_ratings_pitches_talent_forkball',
-      'pitching_ratings_pitches_talent_knuckleball',
-      'pitching_ratings_pitches_talent_knucklecurve',
-      'pitching_ratings_pitches_talent_screwball',
-      'pitching_ratings_pitches_talent_sinker',
-      'pitching_ratings_pitches_talent_slider',
-      'pitching_ratings_pitches_talent_splitter',
-      'pitching_ratings_talent_balk',
-      'pitching_ratings_talent_control',
-      'pitching_ratings_talent_hp',
-      'pitching_ratings_talent_movement',
-      'pitching_ratings_talent_stuff',
-      'pitching_ratings_talent_wild_pitch',
-    ],
-  },
-}
 
 const splits = computed(() => {
   return filteredPlayers.value?.map((player) => {
@@ -253,11 +154,16 @@ const splits = computed(() => {
     <table>
       <thead>
         <tr>
-          <th>name</th>
-          <th>position</th>
-          <th>age</th>
-          <th>team</th>
-          <th>view player</th>
+          <th />
+          <th @click="sortBy = 'name'">
+            name
+          </th>
+          <th @click="sortBy = 'position'">
+            position
+          </th>
+          <th @click="sortBy = 'age'">
+            age
+          </th>
           <th v-for="label of ratingsLabels" :key="label">
             {{ label }}
           </th>
@@ -265,15 +171,14 @@ const splits = computed(() => {
       </thead>
       <tbody>
         <tr v-for="(player, i) of filteredPlayers" :key="player._id">
-          <td>{{ player.last_name }}, {{ player.first_name }}</td>
-          <td>{{ usePositionDisplay(player).value }}</td>
-          <td>{{ player.age }}</td>
-          <td>{{ player.teamId }}</td>
           <td>
             <nuxt-link :to="`/players/${player.player_id}`">
               details
             </nuxt-link>
           </td>
+          <td>{{ player.last_name }}, {{ player.first_name }}</td>
+          <td>{{ usePositionDisplay(player).value }}</td>
+          <td>{{ player.age }}</td>
           <!-- eslint-disable-next-line vue/require-v-for-key -->
           <td v-for="split of splits[i]">
             {{ split }}
