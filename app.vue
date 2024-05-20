cc5a4f1ea374d6a6828f0d97b0bb538e7f8fb822
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2024-05-20 08:19:24 -0600

    css stuff

--- app.vue
+++ app.vue
@@ -13,35 +13,39 @@ async function logout() {
   <ConfigProvider :use-id="idProvider">
     <NuxtLoadingIndicator />
     <main class="page">
-      <nav class="group-between">
-        <div class="group-inline">
-          <NuxtLink class="logo" to="/">
-            NABA League
-          </NuxtLink>
-          <NuxtLink to="/">
-            Home
-          </NuxtLink>
-          <NuxtLink to="/lineups">
-            Lineups
-          </NuxtLink>
-        </div>
-        <div class="group-inline">
-          <template v-if="!user">
-            <NuxtLink to="/auth/login">
-              Login
+      <nav class="border-be">
+        <div class="group-between" style="grid-column: content-start">
+          <div class="group-inline h-full">
+            <NuxtLink class="logo" to="/">
+              NABA League
             </NuxtLink>
-            <NuxtLink to="/auth/signup">
-              Signup
+            <NuxtLink to="/">
+              Home
             </NuxtLink>
-          </template>
-          <template v-else>
-            <NuxtLink v-if="user.role === 'admin'" to="/upload">
-              Upload
+            <NuxtLink to="/lineups">
+              Lineups
             </NuxtLink>
-            <button @click="logout">
-              Logout {{ user.username }}
-            </button>
-          </template>
+            <div class="divider-x" />
+          </div>
+          <div class="group-inline h-full">
+            <div class="divider-x" />
+            <template v-if="!user">
+              <NuxtLink to="/auth/login">
+                Login
+              </NuxtLink>
+              <NuxtLink to="/auth/signup">
+                Signup
+              </NuxtLink>
+            </template>
+            <template v-else>
+              <NuxtLink v-if="user.role === 'admin'" to="/upload">
+                Upload
+              </NuxtLink>
+              <button @click="logout">
+                Logout {{ user.username }}
+              </button>
+            </template>
+          </div>
         </div>
       </nav>
       <section class="content">
