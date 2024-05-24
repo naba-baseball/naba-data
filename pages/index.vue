c4d01323a759605bec1c11c9d091ab92ee9bad88
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2024-05-23 21:19:24 -0600

    fix cards

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,8 @@ const { teams } = useTeams()
       Teams
     </h1>
     <ul role="list" class="card-grid">
-      <li v-for="team in teams || []" :key="team.team_id" class="card">
-        <NuxtLink :to="`/teams/${team.team_id}`">
+      <li v-for="team in teams || []" :key="team.team_id">
+        <NuxtLink :to="`/teams/${team.team_id}`" class="card block">
           <img :src="`https://nabaleague.com/images/team_logos/${team.logo_file_name}`">
           <span> {{ team.name }} {{ team.nickname }} </span>
         </NuxtLink>
